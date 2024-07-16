f27cefdb9bc20cb90c0eb9bc3f776e0b3634bd6d
Usama Husnain <usamahusnain096@gmail.com>
2024-07-16 16:15:48 -0600

    Create the Setup & Requirements File

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages , setup
+from typing import List
+
+# Define the Code for the get requriment function
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readline()
+        requirements = [req.replace("\n" , "") for req in requirements]
+    return requirements
+
+# Define the Parameters for setup
+setup(
+name         =  "Fruad Card Detection",
+version      =  "0.0.1",
+author       =  "Usama Husnain",
+author_email =  "usamahusnain096@gmail.com",
+packages     =  find_packages(),
+requires     =  get_requirements("requirements.txt"),
+)
