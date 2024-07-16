66518a83e4ee430f8ade9a913511024691ad721d
Usama Husnain <usamahusnain096@gmail.com>
2024-07-16 16:24:01 -0600

    Edited the setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from typing import List
 def get_requirements(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as file_obj:
-        requirements = file_obj.readline()
+        requirements = file_obj.readlines()
         requirements = [req.replace("\n" , "") for req in requirements]
     return requirements
 
@@ -16,5 +16,5 @@ version      =  "0.0.1",
 author       =  "Usama Husnain",
 author_email =  "usamahusnain096@gmail.com",
 packages     =  find_packages(),
-requires     =  get_requirements("requirements.txt"),
+install_requires =  get_requirements("requirements.txt"),
 )
