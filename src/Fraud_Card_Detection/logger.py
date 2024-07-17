1c4567482ee6b0a2263c6f02cfff9bc7620caeb3
Usama Husnain <usamahusnain096@gmail.com>
2024-07-17 07:03:01 -0600

    Delete All Files

--- src/Fraud_Card_Detection/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-import os 
-from datetime import datetime
-
-Log_File = f"{datetime.now().strftime("%m__%d__%y , %h__%m__%s")}.log"
-Log_Path = os.path.join(os.getcwd() , "Logs" , Log_File)
-
-print(Log_Path)
